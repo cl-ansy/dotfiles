d0c874dd138149531f99534df53bd542dd7701a6
Chris Lansing <cslansin@gmail.com>
2026-03-06 12:44:26 -0500

    Update nvim packer config

--- home/.config/nvim/lua/clansy/init.lua
+++ home/.config/nvim/lua/clansy/init.lua
@@ -1,8 +1,7 @@
 require("clansy.set")
 require("clansy.remap")
+require("clansy.packer")
 require("clansy.tree")
 require("clansy.airline")
-require("clansy.treesitter")
 require("clansy.telescope")
-require("clansy.lsp")
 
