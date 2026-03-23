5200b41866210885fcf2dec78f4fe0b984555512
Chris Lansing <cslansin@gmail.com>
2026-03-23 10:51:42 -0400

    Update mac deps

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if command -v dnf >/dev/null 2>&1; then
-  sudo dnf install zsh fira-code-fonts vim neovim
+  sudo dnf install zsh fira-code-fonts vim neovim lazygit
 elif command -v brew &>/dev/null 2>&1; then
-  brew install font-fira-code
+  brew install zsh font-fira-code neovim lazygit jq
 fi
