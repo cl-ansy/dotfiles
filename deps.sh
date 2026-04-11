fe95de4bb18d101d6aab58178f402bc8cac0a208
Chris Lansing <cslansin@gmail.com>
2026-04-11 12:51:33 -0400

    Add lazygit copr

--- deps.sh
+++ deps.sh
@@ -2,14 +2,16 @@
 
 common="zsh tmux vim neovim lazygit jq ghostty"
 
-fedora_copr="scottames/ghostty"
+fedora_copr="scottames/ghostty dejan/lazygit"
 fedora="fira-code-fonts"
 
 mac="font-fira-code"
 
 if command -v dnf >/dev/null 2>&1; then
-  sudo dnf copr enable $fedora_copr
-  sudo dnf install $common $fedora
+  for repo in $fedora_copr; do
+    sudo dnf copr enable -y "$repo"
+  done
+  sudo dnf install -y $common $fedora
 elif command -v brew >/dev/null 2>&1; then
   brew install $common $mac
 fi
